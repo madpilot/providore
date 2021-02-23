ef9a14accc6cd622cad0f0b14a2286df99b31f53
Myles Eftos <myles@madpilot.com.au>
2021-02-24 10:01:24 +1100

    Adding command line parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,17 @@
+import { Command } from "commander";
 import { startServer } from "./server";
+const program = new Command();
+program.version("0.0.1");
 
-startServer({ protocol: "https", bind: "0.0.0.0", port: 3000 });
+program.option("-b, --bind <port>", "IP Address to bind to", "0.0.0.0");
+program.option("-p, --port <port>", "TCP port to listen to", "3000");
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+startServer({
+  protocol: "https",
+  bind: options.bind,
+  port: parseInt(options.port),
+});
